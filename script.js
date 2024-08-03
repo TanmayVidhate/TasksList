1b09fefa7d1341b5d86946d66408c28fa3951c8a
TanmayVidhate <tanmayvidhate7@gmail.com>
2024-08-03 18:14:29 +0530

    add update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const tasks = ['hi','by','goo','sleep'];
+const tasks = [];
 
 function loadformdb()
 {
-    const tasks =JSON.parse(localStorage.getItem("alltask"));
+    const alltask =JSON.parse(localStorage.getItem("alltask"));
 
     if(tasks){
-        tasks.push(...tasks)
+        tasks.push(...alltask)
     }
 
     loadtask();
@@ -14,11 +14,10 @@ function loadformdb()
 loadformdb()
 
 
-
-
 function loadtask()
 {
     localStorage.setItem('alltask',JSON.stringify(tasks));
+     
     const taskHolder = document.getElementById('task-holder');
     taskHolder.innerHTML=" "
     for(const task of tasks)
@@ -33,13 +32,25 @@ function loadtask()
     }
 }
 
-
-
 function deletetask(task)
 {
     const workIndex = tasks.indexOf(task);
     tasks.splice(workIndex,1);
 
     loadtask();
+}
+
+function addTask()
+{
+    const taskInput = document.getElementById('task-input');
+    const task = taskInput.value;
+    if(!task){
+        alert("Enter Task First");
+        return
+    }
+
+    tasks.push(task);
+    taskInput.value=" ";
+    loadtask()
 }
 
